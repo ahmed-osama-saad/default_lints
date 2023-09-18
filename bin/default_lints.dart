ef7a0467f0a4affaf9e71ea1c5bc8dc0c1e51c13
ahmed osama <a.osama@trackbar.com>
2023-09-18 12:28:29 +0300

    init

---
+++ bin/default_lints.dart
@@ -0,0 +1,5 @@
+import 'package:default_lints/default_lints.dart' as default_lints;
+
+void main(List<String> arguments) {
+  print('Hello world: ${default_lints.calculate()}!');
+}
---
+++ test/default_lints_test.dart
@@ -0,0 +1,8 @@
+import 'package:default_lints/default_lints.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
